1023d6ebe49c0f833ab9776a8de560de9d1dffcd
issa sarr <issasarr444@gmail.com>
2021-09-10 11:11:21 +0100

    second commit

--- tektal1/src/app/app-routing.module.ts
+++ tektal1/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConnexionComponent } from './nutrition/connexion/connexion.component';
+import { HomeComponent } from './nutrition/home/home.component';
+import { InscriptionComponent } from './nutrition/inscription/inscription.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'inscription', component: InscriptionComponent},
+  { path: 'connexion', component: ConnexionComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- tektal1/src/app/app.module.ts
+++ tektal1/src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './nutrition/header/header.component';
+import { FooterComponent } from './nutrition/footer/footer.component';
+import { ConnexionComponent } from './nutrition/connexion/connexion.component';
+import { InscriptionComponent } from './nutrition/inscription/inscription.component';
+import { HomeComponent } from './nutrition/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ConnexionComponent,
+    InscriptionComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
