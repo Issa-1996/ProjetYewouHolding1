14e5781f8277585730be480513374d1e7531b48a
Aicha Dione <aichadionea@gmail.com>
2021-09-10 12:58:40 +0000

    navbar footer

--- tektal1/src/app/app-routing.module.ts
+++ tektal1/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ConnexionComponent } from './nutrition/connexion/connexion.component';
 import { HomeComponent } from './nutrition/home/home.component';
 import { InscriptionComponent } from './nutrition/inscription/inscription.component';
+import { MdpOublieComponent } from './nutrition/mdp-oublie/mdp-oublie.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'inscription', component: InscriptionComponent},
   { path: 'connexion', component: ConnexionComponent },
+  { path: 'motDePasseOublie', component: MdpOublieComponent},
 ];
 
 @NgModule({
